399183ff2db1f9d2dd7c02cb0d53d3691b753db9
goldenkinstyle <goldenkingstyle@gmail.com>
2024-05-24 17:24:12 +0300

    init frontend

---
+++ client/src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="Header">
+      <div className="header-title">Online compiler</div>
+      <div className="header-how-it-works">How it works?</div>
+    </div>
+  );
+}
